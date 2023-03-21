498af282289f5c213fc57fb3ae476661e4c2f616
CHINMAY PATIL <chinmayp@bluepearlhealthtech.com>
2023-03-21 18:40:06 +0530

    deleted unused code

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,29 +1,12 @@
-import React, { Suspense } from "react";
+import React, { Suspense } from 'react';
 // eslint-disable-next-line no-unused-vars
-import config from "config";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
-import { history } from "@/_helpers";
-import { PrivateRoute, AdminRoute } from "@/_components";
-import { HomePage } from "@/HomePage";
-import { LoginPage } from "@/LoginPage";
-import { SignupPage } from "@/SignupPage";
-import { TooljetDatabase } from "@/TooljetDatabase";
-import { OrganizationInvitationPage } from "@/ConfirmationPage";
-import { Authorize } from "@/Oauth2";
-import { Authorize as Oauth } from "@/Oauth";
-import { Viewer } from "@/Editor";
-import { OrganizationSettings } from "@/OrganizationSettingsPage";
-import { SettingsPage } from "../SettingsPage/SettingsPage";
-import { ForgotPassword } from "@/ForgotPassword";
-import { ResetPassword } from "@/ResetPassword";
-import { MarketplacePage } from "@/MarketplacePage";
-import { lt } from "semver";
-import Toast from "@/_ui/Toast";
-import { VerificationSuccessInfoScreen } from "@/SuccessInfoScreen";
-import "@/_styles/theme.scss";
-import "emoji-mart/css/emoji-mart.css";
-import { AppLoader } from "@/AppLoader";
-import SetupScreenSelfHost from "../SuccessInfoScreen/SetupScreenSelfHost";
+import { BrowserRouter } from 'react-router-dom';
+import { history } from '@/_helpers';
+import { PrivateRoute } from '@/_components';
+import Toast from '@/_ui/Toast';
+import '@/_styles/theme.scss';
+import 'emoji-mart/css/emoji-mart.css';
+import { AppLoader } from '@/AppLoader';
 
 class App extends React.Component {
   constructor(props) {
@@ -32,229 +15,45 @@ class App extends React.Component {
     this.state = {
       currentUser: null,
       fetchedMetadata: false,
-      darkMode: localStorage.getItem("darkMode") === "true",
+      darkMode: localStorage.getItem('darkMode') === 'true',
     };
   }
 
-  fetchMetadata = () => {
-    if (this.state.currentUser) {
-      tooljetService.fetchMetaData().then((data) => {
-        localStorage.setItem("currentVersion", data.installed_version);
-        if (
-          data.latest_version &&
-          lt(data.installed_version, data.latest_version) &&
-          data.version_ignored === false
-        ) {
-          this.setState({ updateAvailable: true });
-        }
-      });
-    }
-  };
-
   componentDidMount() {}
-
-  logout = () => {
-    history.push("/login");
-  };
 
   switchDarkMode = (newMode) => {
     this.setState({ darkMode: newMode });
-    localStorage.setItem("darkMode", newMode);
+    localStorage.setItem('darkMode', newMode);
   };
 
   render() {
-    const { updateAvailable, darkMode } = this.state;
+    const { darkMode } = this.state;
     let toastOptions = {
       style: {
-        wordBreak: "break-all",
+        wordBreak: 'break-all',
       },
     };
 
     if (darkMode) {
       toastOptions = {
-        className: "toast-dark-mode",
+        className: 'toast-dark-mode',
         style: {
-          borderRadius: "10px",
-          background: "#333",
-          color: "#fff",
-          wordBreak: "break-all",
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+          wordBreak: 'break-all',
         },
       };
     }
 
     return (
       <Suspense fallback={null}>
-        <BrowserRouter
-          history={history}
-          basename={window.public_config?.SUB_PATH || "/"}
-        >
-          <div
-            className={`main-wrapper ${darkMode ? "theme-dark" : ""}`}
-            data-cy="main-wrapper"
-          >
-            {updateAvailable && (
-              <div className="alert alert-info alert-dismissible" role="alert">
-                <h3 className="mb-1">Update available</h3>
-                <p>A new version of ToolJet has been released.</p>
-                <div className="btn-list">
-                  <a
-                    href="https://docs.tooljet.io/docs/setup/updating"
-                    target="_blank"
-                    className="btn btn-info"
-                    rel="noreferrer"
-                  >
-                    Read release notes & update
-                  </a>
-                  <a
-                    onClick={() => {
-                      tooljetService.skipVersion();
-                      this.setState({ updateAvailable: false });
-                    }}
-                    className="btn"
-                  >
-                    Skip this version
-                  </a>
-                </div>
-              </div>
-            )}
-
-            <PrivateRoute
-              exact
-              path="/"
-              component={HomePage}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <Route path="/login/:organizationId" exact component={LoginPage} />
-            <Route path="/login" exact component={LoginPage} />
-            <Route
-              path="/setup"
-              exact
-              component={(props) => (
-                <SetupScreenSelfHost {...props} darkMode={darkMode} />
-              )}
-            />
-            <Route path="/sso/:origin/:configId" exact component={Oauth} />
-            <Route path="/sso/:origin" exact component={Oauth} />
-            <Route path="/signup" component={SignupPage} />
-            <Route path="/forgot-password" component={ForgotPassword} />
-            <Route
-              path="/reset-password/:token"
-              render={(props) => (
-                <Redirect
-                  to={{
-                    pathname: "/reset-password",
-                    state: {
-                      token: props.match.params.token,
-                    },
-                  }}
-                />
-              )}
-            />
-            <Route path="/reset-password" component={ResetPassword} />
-            <Route
-              path="/invitations/:token"
-              render={(props) => (
-                <Redirect
-                  to={{
-                    pathname: "/confirm",
-                    state: {
-                      token: props.match.params.token,
-                      search: props.location.search,
-                    },
-                  }}
-                />
-              )}
-            />
-            <Route
-              path="/invitations/:token/workspaces/:organizationToken"
-              render={(props) => (
-                <Redirect
-                  to={{
-                    pathname: "/confirm",
-                    state: {
-                      token: props.match.params.token,
-                      organizationToken: props.match.params.organizationToken,
-                      search: props.location.search,
-                    },
-                  }}
-                />
-              )}
-            />
-            <Route path="/confirm" component={VerificationSuccessInfoScreen} />
-            <Route
-              path="/organization-invitations/:token"
-              render={(props) => (
-                <Redirect
-                  to={{
-                    pathname: "/confirm-invite",
-                    state: {
-                      token: props.match.params.token,
-                      search: props.location.search,
-                    },
-                  }}
-                />
-              )}
-            />
-            <Route
-              path="/confirm-invite"
-              component={(props) => (
-                <OrganizationInvitationPage {...props} darkMode={darkMode} />
-              )}
-            />
+        <BrowserRouter history={history} basename={window.public_config?.SUB_PATH || '/'}>
+          <div className={`main-wrapper ${darkMode ? 'theme-dark' : ''}`} data-cy="main-wrapper">
             <PrivateRoute
               exact
               path="/apps/:id/:pageHandle?"
               component={AppLoader}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/applications/:id/versions/:versionId/:pageHandle?"
-              component={Viewer}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/applications/:slug/:pageHandle?"
-              component={Viewer}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/oauth2/authorize"
-              component={Authorize}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/workspace-settings"
-              component={OrganizationSettings}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/settings"
-              component={SettingsPage}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <PrivateRoute
-              exact
-              path="/database"
-              component={TooljetDatabase}
-              switchDarkMode={this.switchDarkMode}
-              darkMode={darkMode}
-            />
-            <AdminRoute
-              exact
-              path="/integrations"
-              component={MarketplacePage}
               switchDarkMode={this.switchDarkMode}
               darkMode={darkMode}
             />
--- src/AppLoader/AppLoader.jsx
+++ src/AppLoader/AppLoader.jsx
@@ -1,37 +1,8 @@
-import React, { useEffect } from "react";
-import { withTranslation } from "react-i18next";
-import { Editor } from "../Editor/Editor";
-import { RealtimeEditor } from "@/Editor/RealtimeEditor";
-import config from "config";
-import { safelyParseJSON, stripTrailingSlash } from "@/_helpers/utils";
-import { toast } from "react-hot-toast";
-import useRouter from "@/_hooks/use-router";
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import { Editor } from '../Editor/Editor';
 
 const AppLoaderComponent = (props) => {
-  const router = useRouter();
-  const appId = props.match.params.id;
-
-  const handleError = (error) => {
-    try {
-      if (error?.data) {
-        const statusCode = error.data?.statusCode;
-        if (statusCode === 403) {
-          const errorObj = safelyParseJSON(error.data?.message);
-          // if (errorObj?.organizationId && currentUser.organization_id !== errorObj?.organizationId) {
-          //   switchOrganization(errorObj?.organizationId);
-          //   return;
-          // }
-          return router.push("/");
-        } else if (statusCode === 404 || statusCode === 422) {
-          toast.error(error?.error ?? "App not found");
-        }
-        return router.push("/");
-      }
-    } catch (err) {
-      return router.push("/");
-    }
-  };
-
   return <Editor {...props} />;
 };
 
