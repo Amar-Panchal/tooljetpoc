619a853167e194078ebd29b24cc513b45ab63a16
CHINMAY PATIL <chinmayp@bluepearlhealthtech.com>
2023-04-07 10:44:03 +0530

    minor changs

--- src/Editor/RegistrationPage/Components/KendoButton.jsx
+++ src/Editor/RegistrationPage/Components/KendoButton.jsx
@@ -4,10 +4,9 @@ import { Button } from '@progress/kendo-react-all';
 import React from 'react';
 
 function KendoButton({ onclick, props }) {
-  console.log('kendobutton', props);
   return (
     <Button themeColor={'primary'} onClick={onclick}>
-      Primary
+      {props.name}
     </Button>
   );
 }
