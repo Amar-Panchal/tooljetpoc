20430ae34217f8725dd1ebb280d2a0b94f37c13e
CHINMAY PATIL <chinmayp@bluepearlhealthtech.com>
2023-04-26 17:29:10 +0530

    added image for result

--- src/Editor/ResultPage/ResultPage.jsx
+++ src/Editor/ResultPage/ResultPage.jsx
@@ -134,147 +134,195 @@ function ResultPage() {
   };
 
   return (
-    <div style={{ width: "100%", padding: "20px" }}>
-      <h1>Result</h1>
+    <div style={{ border: "1px solid transparent" }}>
       <div
         style={{
-          border: "1px solid gray",
-          padding: "20px",
-          borderRadius: "15px",
+          position: "fixed",
+          width: "58px",
+          height: "100vh",
+          top: "45px",
         }}
       >
-        <div>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              // flexWrap: "wrap",
-              justifyContent: "space-evenly",
-              padding: "20px",
-              border: "1px dotted gray",
-              borderRadius: "15px",
-              flexDirection: "column",
-            }}
-          >
-            <div style={{ padding: "10px" }}>
-              <h1>Patient Details</h1>
-            </div>
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <div>
-                <b> Patient Name :</b> {patientDetails?.patientName}
-              </div>
-              <div>
-                <b>Age:</b> {patientDetails?.age}
-              </div>
-              <div>
-                <b>Gender :</b> {patientDetails?.gender?.name}
-              </div>
-              <div>
-                <b>Membership Type</b> : {patientDetails?.membershipType?.label}
-              </div>
-              <div>
-                <b>Referred Doctor</b> : {patientDetails?.referredDoctor}
-              </div>
-            </div>
-          </div>
+        <img
+          src="https://elabnextstorage.blob.core.windows.net/test/image_2023_04_25T07_21_59_615Z_Default_947499017.png"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "50px",
+        }}
+      >
+        <img
+          src="https://elabnextstorage.blob.core.windows.net/test/elab%20fial%202_Default_393598438.JPG"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+      <div
+        style={{
+          width: "95%",
+          padding: "20px",
+          marginTop: "30px",
+          marginLeft: "50px",
+          height: "100%",
+          minHeight: "100vh",
+        }}
+      >
+        <div
+          style={{
+            fontStyle: "normal",
+            fontSize: "30px",
+            lineHeight: "50px",
+            display: "flex",
+            alignItems: "center",
+            color: "#212529",
+            marginLeft: "30px",
+          }}
+        >
+          Result
         </div>
         <div
           style={{
-            marginTop: "20px",
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-
             padding: "20px",
-            border: "1px dotted gray",
             borderRadius: "15px",
           }}
         >
           <div>
-            <h1>Selected Tests</h1>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              gap: "10px",
-              justifyContent: "space-evenly",
-            }}
-          >
-            {patientDetails?.selectedTests?.map((test) => {
-              return (
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                // flexWrap: "wrap",
+                justifyContent: "space-evenly",
+                padding: "20px",
+                border: "1px dotted gray",
+                borderRadius: "15px",
+                flexDirection: "column",
+              }}
+            >
+              <div style={{ padding: "10px" }}>
+                <h1>Patient Details</h1>
+              </div>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                }}
+              >
                 <div>
-                  <Checkbox
-                    name={JSON.stringify(test)}
-                    label={test?.testName}
-                    onChange={onTestChange}
-                  />
+                  <b> Patient Name :</b> {patientDetails?.patientName}
                 </div>
-              );
-            })}
+                <div>
+                  <b>Age:</b> {patientDetails?.age}
+                </div>
+                <div>
+                  <b>Gender :</b> {patientDetails?.gender?.name}
+                </div>
+                <div>
+                  <b>Membership Type</b> :{" "}
+                  {patientDetails?.membershipType?.label}
+                </div>
+                <div>
+                  <b>Referred Doctor</b> : {patientDetails?.referredDoctor}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div
-          style={{
-            marginTop: "20px",
-          }}
-        >
           <div
             style={{
-              border: "1px dotted gray",
+              marginTop: "20px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+
               padding: "20px",
+              border: "1px dotted gray",
               borderRadius: "15px",
             }}
           >
-            <h1>Parameters</h1>
             <div>
-              {selectedTestsWithParameters.map((test) => {
+              <h1>Selected Tests</h1>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+                justifyContent: "space-evenly",
+              }}
+            >
+              {patientDetails?.selectedTests?.map((test) => {
                 return (
-                  <div
-                    style={{
-                      border: "1px dotted gray",
-                      padding: "15px",
-                      margin: "10px",
-                      borderRadius: "15px",
-                    }}
-                  >
-                    <h3>{test.testName}</h3>
-                    {test.testParameters.map((parameter) => {
-                      return (
-                        <RenderParameterList
-                          parameterName={parameter}
-                          values={values}
-                          setValues={setValues}
-                        />
-                      );
-                    })}
+                  <div>
+                    <Checkbox
+                      name={JSON.stringify(test)}
+                      label={test?.testName}
+                      onChange={onTestChange}
+                    />
                   </div>
                 );
               })}
             </div>
           </div>
+        </div>
+        <div>
           <div
             style={{
-              border: "1px dotted gray",
-              padding: "15px",
-              marginTop: "10px",
-              display: "flex",
-
-              borderRadius: "15px",
-              justifyContent: "space-evenly",
+              marginTop: "20px",
             }}
           >
-            <Button themeColor="primary" onClick={handleSubmitResult}>
-              Submit
-            </Button>
-            <Button>Cancel</Button>
+            <div
+              style={{
+                border: "1px dotted gray",
+                padding: "20px",
+                borderRadius: "15px",
+              }}
+            >
+              <h1>Parameters</h1>
+              <div>
+                {selectedTestsWithParameters.map((test) => {
+                  return (
+                    <div
+                      style={{
+                        border: "1px dotted gray",
+                        padding: "15px",
+                        margin: "10px",
+                        borderRadius: "15px",
+                      }}
+                    >
+                      <h3>{test.testName}</h3>
+                      {test.testParameters.map((parameter) => {
+                        return (
+                          <RenderParameterList
+                            parameterName={parameter}
+                            values={values}
+                            setValues={setValues}
+                          />
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div
+              style={{
+                border: "1px dotted gray",
+                padding: "15px",
+                marginTop: "10px",
+                display: "flex",
+
+                borderRadius: "15px",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Button themeColor="primary" onClick={handleSubmitResult}>
+                Submit
+              </Button>
+              <Button>Cancel</Button>
+            </div>
           </div>
         </div>
       </div>
