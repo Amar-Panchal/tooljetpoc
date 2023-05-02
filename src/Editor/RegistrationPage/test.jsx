014997454f999b777bf858d34132400d58b902b9
CHINMAY PATIL <chinmayp@bluepearlhealthtech.com>
2023-05-02 13:36:13 +0530

    css changes incomplete

--- src/Editor/RegistrationPage/test.jsx
+++ src/Editor/RegistrationPage/test.jsx
@@ -5,6 +5,16 @@ import axios from "axios";
 import "./registration-page.css";
 import { Input } from "@progress/kendo-react-all";
 
+import { Button } from "@progress/kendo-react-buttons";
+import { Icon } from "@progress/kendo-react-common";
+import {
+  InputClearValue,
+  InputPrefix,
+  InputSeparator,
+  InputSuffix,
+  TextBox,
+} from "@progress/kendo-react-inputs";
+
 function SelectTests({
   setPatientRegistrationFormData,
   PatientRegistrationFormData,
@@ -65,92 +75,67 @@ function SelectTests({
     setTestListFiltered(filteredList);
   }, [searchQuery]);
 
-  console.log("colorrrrr", onHoverColor);
   return (
-    <div style={{ height: "100%", overflowY: "scroll" }}>
-      <div
-        style={{
-          // border: "1px solid #EBEBEB",
-          width: "30%",
-          display: "flex",
-          gap: "5px",
-          alignItems: "center",
-          borderRadius: "10px",
-          margin: "5px",
-        }}
-      >
-        <span className="k-icon k-i-zoom"></span>
-
-        <Input
-          value={searchQuery}
-          onChange={handleSearchQuery}
-          placeholder="Search Test Name"
-          style={{
-            border: "none",
-          }}
-        />
-      </div>
-      <div
-        style={{
-          overflowY: "scroll",
-          overflow: "hidden",
-          display: "flex",
-          width: "100%",
-          padding: "10px",
-          backgroundColor: "none",
-          color: textColor,
-        }}
-      >
-        <div
-          style={{
-            overflowY: "scroll",
-            width: "50%",
-            cursor: "pointer",
-          }}
-        >
+    <div style={{ height: "100%", padding: "10px" }}>
+      <TextBox
+        value={searchQuery}
+        onChange={handleSearchQuery}
+        placeholder="Search Test Name"
+        prefix={() => (
+          <>
+            <InputPrefix>
+              <span className="k-icon k-i-zoom"></span>
+            </InputPrefix>
+            <InputSeparator />
+          </>
+        )}
+        style={{ width: "50%", height: "20%" }}
+      />
+      <div style={{ height: "90%", display: "flex" }}>
+        <div style={{ width: "50%", height: "90%" }}>
           <h3>Test List</h3>
-          {testListFiltered.length > 0 && (
-            <div>
-              {testListFiltered.map((test) => {
-                return (
-                  <div
-                    onClick={() => handleItemClick(test)}
-                    key={test.testId}
-                    style={{
-                      color: textColor,
-                      backgroundColor: backgroundColor,
-                    }}
-                    onMouseEnter={(event) => {
-                      event.target.style.backgroundColor = onHoverColor;
-                    }}
-                    onMouseLeave={(event) => {
-                      event.target.style.backgroundColor = backgroundColor;
-                    }}
-                  >
-                    {` ${test.testName} (${
-                      test.shortName ? test.shortName : ""
-                    } ) `}
-                  </div>
-                );
-              })}
-            </div>
-          )}
+          <div
+            style={{
+              height: "90%",
+              overflow: "scroll",
+            }}
+          >
+            {testListFiltered.map((test) => {
+              return (
+                <div
+                  onClick={() => handleItemClick(test)}
+                  key={test.testId}
+                  style={{
+                    color: textColor,
+                    backgroundColor: backgroundColor,
+                  }}
+                  onMouseEnter={(event) => {
+                    event.target.style.backgroundColor = onHoverColor;
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.backgroundColor = backgroundColor;
+                  }}
+                >
+                  {` ${test.testName} (${
+                    test.shortName ? test.shortName : ""
+                  } ) `}
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div
-          style={{
-            overflowY: "scroll",
-            width: "50%",
-            cursor: "pointer",
-          }}
-        >
+        <div style={{ width: "50%", height: "90%" }}>
           <h3>Selected Test List</h3>
-          {selectedTests.length > 0 && (
-            <div>
-              {selectedTests.map((test) => {
-                return <div className="selected-test">{test.testName}</div>;
-              })}
-            </div>
-          )}
+          <div
+            style={{
+              overflow: "scroll",
+              height: "90%",
+            }}
+          >
+            {selectedTests.map((test) => {
+              return <div>{test.testName}</div>;
+            })}
+          </div>
         </div>
       </div>
     </div>
