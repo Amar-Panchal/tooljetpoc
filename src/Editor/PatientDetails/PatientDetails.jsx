eb4a0b77bf5b42271885b64925dc4a5874c66f3d
CHINMAY PATIL <chinmayp@bluepearlhealthtech.com>
2023-05-02 18:58:37 +0530

    css remove patient details

--- src/Editor/PatientDetails/PatientDetails.jsx
+++ src/Editor/PatientDetails/PatientDetails.jsx
@@ -15,7 +15,7 @@ const initialDataState = {
       dir: "asc",
     },
   ],
-  take: 50,
+  take: 20,
   skip: 0,
 };
 
@@ -128,88 +128,35 @@ function PatientDetails() {
 
   return (
     <div>
-      <div
-        style={{
-          position: "fixed",
-          width: "58px",
-          height: "100vh",
-          top: "45px",
+      <Grid
+        resizable={true}
+        pageable={true}
+        sortable={true}
+        filterable={true}
+        reorderable={true}
+        data={process(PatientDetailsList, dataState)}
+        {...dataState}
+        onDataStateChange={(e) => {
+          setDataState(e.dataState);
         }}
+        onColumnReorder={handleColumnReorder}
+        GridEvent={(event) => console.log("eve", event)}
+        style={{ minHeight: "100%" }}
       >
-        <img
-          src="https://elabnextstorage.blob.core.windows.net/test/image_2023_04_25T07_21_59_615Z_Default_947499017.png"
-          style={{ width: "100%", height: "100%" }}
-        />
-      </div>
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "50px",
-        }}
-      >
-        <img
-          src="https://elabnextstorage.blob.core.windows.net/test/elab%20fial%202_Default_393598438.JPG"
-          style={{ width: "100%", height: "100%", zIndex: "9999" }}
-        />
-      </div>
-      <div
-        style={{
-          position: "fixed",
-          width: "95%",
-          height: "100vh",
-          top: "60px",
-          left: "60px",
-        }}
-        // className="patient-details-layout"
-      >
-        <div
-          style={{
-            fontStyle: "normal",
-
-            fontSize: "30px",
-            lineHeight: "50px",
-            display: "flex",
-            alignItems: "center",
-            color: "#212529",
-            marginLeft: "50px",
-          }}
-        >
-          Patient Details
-        </div>
-        <div>
-          <Grid
-            resizable={true}
-            pageable={true}
-            sortable={true}
-            filterable={true}
-            reorderable={true}
-            style={{
-              height: "80vh",
-              marginTop: "20px",
-            }}
-            data={process(PatientDetailsList, dataState)}
-            {...dataState}
-            onDataStateChange={(e) => {
-              setDataState(e.dataState);
-            }}
-            onColumnReorder={handleColumnReorder}
-            GridEvent={(event) => console.log("eve", event)}
-          >
-            {keysForGrid?.map((key) => {
-              return fieldMasterList.map((field) => {
-                if (field.value === key) {
-                  return createGridColumn(field);
-                }
-              });
-            })}
-            <GridColumn
-              field="dd"
-              title="Actions"
-              cell={(props) => {
-                return (
-                  <td style={{ display: "flex", gap: "10px" }}>
-                    {/* <button
+        {keysForGrid?.map((key) => {
+          return fieldMasterList.map((field) => {
+            if (field.value === key) {
+              return createGridColumn(field);
+            }
+          });
+        })}
+        <GridColumn
+          field="dd"
+          title="Actions"
+          cell={(props) => {
+            return (
+              <td>
+                {/* <button
                     onClick={() =>
                       history.push({
                         pathname: "/",
@@ -227,23 +174,21 @@ function PatientDetails() {
                   >
                     <span class="k-icon k-i-edit"></span>
                   </button> */}
-                    <button
-                      onClick={() =>
-                        history.push({
-                          pathname: "/result",
-                          state: props.dataItem,
-                        })
-                      }
-                    >
-                      <span class="k-icon k-i-arrow-double-60-right"></span>
-                    </button>
-                  </td>
-                );
-              }}
-            />
-          </Grid>
-        </div>
-      </div>
+                <button
+                  onClick={() =>
+                    history.push({
+                      pathname: "/result",
+                      state: props.dataItem,
+                    })
+                  }
+                >
+                  <span class="k-icon k-i-arrow-double-60-right"></span>
+                </button>
+              </td>
+            );
+          }}
+        />
+      </Grid>
     </div>
   );
 }
